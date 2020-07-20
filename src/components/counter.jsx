bc04feeb480653122c48d0d0fc6566fd4cb4bc84
Karishma-Joseph <karishmajoseph70@gmail.com>
2020-07-19 20:57:12 -0700

    Implement game

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,9 @@ import "./counter.css";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    level: 1,
+    isButtonStart: true,
+    interval: null,
     bulbs: [
       { id: 0, color: "yellow", name: "zero" },
       { id: 1, color: "yellow", name: "one" },
@@ -26,39 +28,58 @@ class Counter extends Component {
 
   changeBulbColor() {
     let i = 0;
-    setInterval(() => {
-      this.setState({
-        bulbs: this.state.bulbs.map((bulb) => {
-          bulb.color = bulb.id === i % 12 ? "red" : "yellow";
-          return bulb;
-        }),
-      });
-      i++;
-    }, 1000);
+
+    //If button is STOP, clear interval
+    if (!this.state.isButtonStart) {
+      clearInterval(this.state.interval);
+      if (this.state.bulbs[6].color === "yellow") {
+        this.setState({ level: 1 });
+      } else {
+        this.setState({ level: this.state.level + 1 });
+      }
+    }
+
+    //Toggle button
+    this.setState({
+      isButtonStart: !this.state.isButtonStart,
+    });
+
+    //If button is START, start movement
+    if (this.state.isButtonStart) {
+      let interval = setInterval(() => {
+        this.setState({
+          bulbs: this.state.bulbs.map((bulb) => {
+            bulb.color = bulb.id === i % 12 ? "red" : "yellow";
+            return bulb;
+          }),
+        });
+        i++;
+      }, 1000 / this.state.level);
+      this.setState({ interval });
+    }
   }
 
   render() {
     return (
       <span className="rounded-circle bg-dark divStyle">
-        <span className={this.getBadgeClasses(0)}>{this.state.count}</span>
-        <span className={this.getBadgeClasses(1)}>{this.state.count}</span>
-        <span className={this.getBadgeClasses(2)}>{this.state.count}</span>
-        <span className={this.getBadgeClasses(3)}>{this.state.count}</span>
-        <span className={this.getBadgeClasses(4)}>{this.state.count}</span>
-        <span className={this.getBadgeClasses(5)}>{this.state.count}</span>
-        <span className={this.getBadgeClasses(6)}>{this.state.count}</span>
-        <span className={this.getBadgeClasses(7)}>{this.state.count}</span>
-        <span className={this.getBadgeClasses(8)}>{this.state.count}</span>
-        <span className={this.getBadgeClasses(9)}>{this.state.count}</span>
-        <span className={this.getBadgeClasses(10)}>{this.state.count}</span>
-        <span className={this.getBadgeClasses(11)}>{this.state.count}</span>
+        <span className={this.getBadgeClasses(0)}>{this.state.level}</span>
+        <span className={this.getBadgeClasses(1)}>{this.state.level}</span>
+        <span className={this.getBadgeClasses(2)}>{this.state.level}</span>
+        <span className={this.getBadgeClasses(3)}>{this.state.level}</span>
+        <span className={this.getBadgeClasses(4)}>{this.state.level}</span>
+        <span className={this.getBadgeClasses(5)}>{this.state.level}</span>
+        <span className={this.getBadgeClasses(6)}>{this.state.level}</span>
+        <span className={this.getBadgeClasses(7)}>{this.state.level}</span>
+        <span className={this.getBadgeClasses(8)}>{this.state.level}</span>
+        <span className={this.getBadgeClasses(9)}>{this.state.level}</span>
+        <span className={this.getBadgeClasses(10)}>{this.state.level}</span>
+        <span className={this.getBadgeClasses(11)}>{this.state.level}</span>
         <button
           style={{ top: "120%", left: "33%" }}
           onClick={this.changeBulbColor}
           className="position-absolute btn btn-secondary"
         >
-          {" "}
-          Increment{" "}
+          {this.state.isButtonStart ? "START" : "STOP"}
         </button>
       </span>
     );
